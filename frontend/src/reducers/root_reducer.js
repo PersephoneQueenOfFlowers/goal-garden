e5cea1bce508d9530279f117f346e382dac602a2
Andrew Curd <andrewc2695@gmail.com>
2021-02-22 10:27:16 -0800

    changed root reducer to reflect our data

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import session from './session_api_reducer.js';
 import errors from './errors_reducer'
-import tweets from './tweets_reducer'
 
 const RootReducer = combineReducers({
     session,
     errors,
-    tweets
 });
 
 export default RootReducer;
