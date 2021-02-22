bc5dcd3c439560468a1ad770a573a64d71462b76
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2021-02-22 12:11:14 -0800

    Add work on post goals route

--- goals.js
+++ goals.js
@@ -24,10 +24,15 @@ const keys = require('../../config/keys');
 
 // goal-index route: GET
 // should only be accessible when a user is authenticated
-// should return all names + goal status + id's (for linking to goal show page)
+// should return all names + goal status + id's (for linking to goal show page) -- for now just send whole goals down
 router.get("/", (req, res) => {
   //also is test route for now
   res.json({ msg: "This is the goal route" });
+  //  Goal
+  //   .find()
+  //   .sort({ date: -1 })
+  //   .then(goals => res.json(goals))
+  //   .catch(err => res.status(400).json(err));
 });
 
 
@@ -35,28 +40,48 @@ router.get("/", (req, res) => {
 // should only be accessible when a user is authenticated and current goal belongs to user
 // should return all information about the 1 goal 
 // (later) should return (maybe all? maybe just first x, or first 10) journals associated with goal
-router.get("/:id", (req, res) => {
-  
+router.get("/:id", 
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Goal
+      .findById(req.params.id)
+      .then(goal => res.json(goal))
+      .catch(err => res.status(400).json(err));
 });
 
 // goal-create route: POST
 // should only be accesible when a user is authenticated and current goal belongs to user
 // should only save goal if it passes validations
-router.post("/:id", (req, res) => {
+router.post("/:id", 
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
   
+      const { isValid, errors } = validateGoalInput(req.body);
+
+      if (!isValid) {
+        return res.status(400).json(errors);
+      }
+
+      const newGoal = new Goal ({
+
+      });
 });
 
 // goal-update route: PATCH
 // should only be accesible when a user is authenticated and current goal belongs to user
 // should only be able to modify goal attributes (not userId or journalIds -- unless )
-router.patch("/:id", (req, res) => {
+router.patch("/:id",  
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
   
 });
 
 // goal-delete route: DESTROY
 // should only be accesible when a user is authenticated and current goal belongs to user
 // destroys all associated journals
-router.destroy("/:id", (req, res) => {
+router.destroy("/:id",  
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
   
 });
 
