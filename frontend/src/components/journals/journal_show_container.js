85d7bcfe65a974299173427061dbeb7fc46276f4
Andrew Curd <andrewc2695@gmail.com>
2021-02-23 10:13:13 -0800

    wrote skeleton for journals container

---
+++ frontend/src/components/journals/journal_show_container.js
@@ -0,0 +1,17 @@
+import { connect } from "react-redux"
+import { fetchJournal } from "../../actions/journal_actions"
+
+
+const mapStateToProps = (state, ownProps) => {
+    return{
+        
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return{
+        
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(JournalComponent)
