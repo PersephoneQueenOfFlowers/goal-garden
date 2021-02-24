77b75b9876349c47cf08b08db72574b325261940
SediqaFahimi <sediqa.fahimi@gmail.com>
2021-02-23 22:56:29 -0800

    accepted journals in root reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,21 +1,14 @@
 import { combineReducers } from 'redux';
 import session from './session_api_reducer.js';
-<<<<<<< HEAD
 import errors from './errors_reducer';
 import goals from './goals_reducer';
-=======
-import errors from './errors_reducer'
-import journal from './journals_reducer'
->>>>>>> master
+import journal from './journals_reducer';
 
 const RootReducer = combineReducers({
     session,
     errors,
-<<<<<<< HEAD
-    goals
-=======
-    journal,
->>>>>>> master
+    goals,
+    journal
 });
 
 export default RootReducer;
