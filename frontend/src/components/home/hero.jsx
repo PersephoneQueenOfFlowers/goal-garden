22e0eb561b869549dfeb9f71dedc28ba53fdc11e
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-02-23 21:03:59 -0800

    buttons, fixed hero and nav issue

--- frontend/src/components/home/hero.jsx
+++ frontend/src/components/home/hero.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav from '../nav/nav';
 
 function Hero(props){
 
   return (
     <section className="hero">
+      <Nav />
       <div className="hero-inner-container">
         <h1><span className="bodoni">G</span><span className="beauty">G</span>oal<span className="beauty">G</span>arden<span className="bodoni">F</span></h1>
       </div>
