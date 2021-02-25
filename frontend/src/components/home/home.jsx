9a0b8e92c5b71fa8fe0bd5f78b28092930c0cd91
Reed Haubenstock <rhaubenstock@users.noreply.github.com>
2021-02-24 17:10:00 -0800

    Modifies home.jsx so journal entries can be added

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -2,20 +2,80 @@ import React, { Component } from 'react'
 import Hero from './hero';
 import Modal from 'react-bootstrap/Modal';
 
-let modalContent = {body: "", journals: []};
+const goals = {
+  clean: {
+    body: "clean out the garage",
+    journals: [
+      "First, I'll get all the old rags and clothes, recycle and donate them.",
+      "then I have to either fix the old truck sitting there, or get rid of it",
+      "rearrange the tool area",
+      "hang up storage",
+      "redo the floor"
+    ]
+  },
+  build: {
+    body: "build a new dog house",
+    journals: [
+      "First, I'll need to go on youtube and find the tutorial that makes the most sense, watch that and take some next steps notes.",
+      "Make a materials, list, then get out to the lumber store and buy all the materials",
+      "Plan for a day when it's dry out and get the thing built"
+    ]
+  },
+  go: {
+    body: "go shopping",
+    journals: [
+      "Get some food"
+    ]
+  },
+  start: {
+    body: "start on the novel",
+    journals: ["decide on a topic",
+      "make an outline",
+      "write a two-page version and see if I want to do more"
+    ]
+  },
+  paint: {
+    body: "paint the kitchen",
+    journals: [
+      "Decide on a color, get a consultation if I can't make up my mind",
+      "Make a materials list, then get out to the paint store and buy all the materials",
+      "Plan for a day when it's dry so the paint dries and get the thing painted"
+    ]
+  }
+};
 
 class MyVerticallyCenteredModal extends React.Component {
   constructor(props){
     super(props)
     this.props = props
-    this.state = this.props.modalContent
-    this.state.journal_entry = ""
-    this.addJournal = this.addJournal.bind(this);
+    this.state = {};
+    this.state.goals = goals;
+    this.state.key = this.props.modalkey;
+    this.state.goal = this.state.goals[this.props.modalkey];
+    this.state.journal_entry = "";
+  }
+
+  componentDidUpdate(){
+    if(this.state.key !== this.props.modalkey){
+      this.setState({
+        key: this.props.modalkey,
+        goal: this.state.goals[this.props.modalkey],
+        journal_entry: ""
+      })
+    }
   }
 
   addJournal(e){
     e.preventDefault();
-    debugger
+    const curr_goal = this.state.goals[this.state.key];
+    curr_goal.journals.push(this.state.journal_entry);
+    this.setState({ journal_entry: "" });
+  }
+
+  updateField(field){
+    return (e) => {
+      this.setState({ [field]: e.currentTarget.value })
+    }
   }
 
   render(){
@@ -33,13 +93,17 @@ class MyVerticallyCenteredModal extends React.Component {
         </Modal.Header>
         <Modal.Body>  
 
-          <h4>{modalContent.body}</h4>
-          {modalContent.journals.map(function (journal) {
-            return <p>{journal}</p>;
+          <h4>{this.state.goal.body}</h4>
+          {this.state.goal.journals.map(function (journal, idx) {
+            return <p key={idx}>{journal}</p>;
           })}
         
             <form>
-              <input type="textarea" value={this.state.journal_entry}/>
+              <input 
+                type="textarea" 
+                value={this.state.journal_entry}
+                onChange={this.updateField("journal_entry")}
+              />
               <input 
                 type="button" 
                 value="add journal"
@@ -59,53 +123,12 @@ class MyVerticallyCenteredModal extends React.Component {
 const Home = (props) => {
 
   const [modalShow, setModalShow] = React.useState(false);
+  const [modalKey, setModalKey] = React.useState('clean');
 
   const setModal = (e) => {
     e.preventDefault();
 
-    const modalGoals = {
-      clean: {
-        body: "clean out the garage",
-        journals: [
-          "First, I'll get all the old rags and clothes, recycle and donate them.",
-          "then I have to either fix the old truck sitting there, or get rid of it",
-          "rearrange the tool area",
-          "hang up storage",
-          "redo the floor"
-        ]
-      },
-      build: {
-        body: "build a new dog house",
-        journals: [
-          "First, I'll need to go on youtube and find the tutorial that makes the most sense, watch that and take some next steps notes.",
-          "Make a materials, list, then get out to the lumber store and buy all the materials",
-          "Plan for a day when it's dry out and get the thing built"
-        ]
-      },
-      go: {
-        body: "go shopping",
-        journals: [
-          "Get some food"
-        ]
-      },
-      start: {
-        body: "start on the novel",
-        journals: ["decide on a topic",
-          "make an outline",
-          "write a two-page version and see if I want to do more"
-        ]
-      },
-      paint: {
-        body: "paint the kitchen",
-        journals: [
-          "Decide on a color, get a consultation if I can't make up my mind",
-          "Make a materials list, then get out to the paint store and buy all the materials",
-          "Plan for a day when it's dry so the paint dries and get the thing painted"
-        ]
-      }
-    } 
-
-    modalContent = modalGoals[e.target.value];
+    setModalKey(e.target.value);
     setModalShow(true);
   }
 
@@ -133,11 +156,18 @@ const Home = (props) => {
           <div className="goals-container">
             <h3>Current Goals</h3>
             <ul className="goal-list">
-              <li className="goalTitle"><button value="clean" onClick={(e) => setModal(e)}>clean out the garage</button></li>
-              <li className="goalTitle build"><button value="build" onClick={(e) => setModal(e)}>build a new dog house</button></li>
-              <li className="goalTitle go"><button value="go" onClick={(e) => setModal(e)}>go shopping</button></li>
-              <li className="goalTitle start"><button value="start" onClick={(e) => setModal(e)}>start on the novel</button></li>
-              <li className="goalTitle paint"><button value="paint" onClick={(e) => setModal(e)}>paint the kitchen</button></li>
+              {
+                Object.keys(goals).map( (goalKey, idx) =>{
+                  const goal = goals[goalKey];
+                  return (
+                    <li key={idx} className={`goalTitle ${goalKey}`}>
+                      <button value={goalKey} onClick={(e) => setModal(e)}>
+                        {goal.body}
+                      </button>
+                    </li>
+                  )
+                })
+              }
             </ul>
           </div>
         </div>
@@ -145,7 +175,7 @@ const Home = (props) => {
       <MyVerticallyCenteredModal
         show={modalShow}
         onHide={() => setModalShow(false)}
-        modalContent={modalContent}
+        modalkey={modalKey}
       />
     </div>
   )
