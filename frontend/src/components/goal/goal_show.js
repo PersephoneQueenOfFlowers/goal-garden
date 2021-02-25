e6d86bd40f14edc08d236b4b315558e9b26fc1ad
seth_schoenfeld <sethschoenfeld@gmail.com>
2021-02-25 10:45:46 -0800

    created background for goal show page by accident

--- frontend/src/components/goal/goal_show.js
+++ frontend/src/components/goal/goal_show.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import Hero from '../home/hero';
 
 class GoalShow extends React.Component{
     constructor(props){
         super(props);
 
     }
+
+//     <div>
+//   <h2>{goal.title}</h2>
+//   <p>{goal.body}</p>
+//   <div>Journals
+//                     {journalsArr.map(journal => {
+//     return (<div>{journal.body}</div>)
+//   })}
+//   </div>
+// </div>
     componentDidMount(){
         this.props.fetchGoal(this.props.match.params.goalId);
         this.props.fetchJournals(this.props.match.params.goalId)
@@ -18,18 +29,33 @@ class GoalShow extends React.Component{
         let journalsArr = []
         if(journals !== undefined){
             journalsArr = Object.values(journals)
-            debugger
         }
         return(
-           <div>
-               <h2>{goal.title}</h2>
-               <p>{goal.body}</p>
-               <div>Journals
-                    {journalsArr.map(journal => {
-                        return (<div>{journal.body}</div>)
-                    })}
-               </div>
-           </div>
+          <div>
+          <Hero />
+            <section className="middle taskList">
+              <img src='./css/images/grass-border.png' alt=""/>
+              <div className="left">
+                <div className="background-container">
+                  <h3>
+                    <h2>{goal.title}</h2>
+                </h3>
+                  <p>{goal.body}</p>
+                </div>
+              </div>
+              <div className="right">
+                <div className="goals-container">
+                  <h3>Journals</h3>
+                  <ul className="goal-list">
+                    {
+                      journalsArr.map(journal => {
+                        return (<li>{journal.body}</li>)
+                      })}
+                  </ul>
+                </div>
+              </div>
+            </section>
+          </div>
         )
     }
 }
