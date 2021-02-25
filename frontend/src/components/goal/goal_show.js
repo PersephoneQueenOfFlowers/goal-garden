893efbabe8739503f56fdefdb3e54faf4771f7e7
Andrew Curd <andrewc2695@gmail.com>
2021-02-25 15:59:29 -0800

    removed commented otu code

--- frontend/src/components/goal/goal_show.js
+++ frontend/src/components/goal/goal_show.js
@@ -70,63 +70,7 @@ class GoalShow extends React.Component{
             journalsArr = Object.values(journals)
         }
         return(
-        //    <div className="goal_show_component" >
-        //         <div id={`growth${this.growth}`} className="flower_picture"></div>
-        //        <div className="goal_show_info">
-        //             <h2>{goal.title}</h2>
-        //             <p>{goal.body}</p>
-        //             <div>Journals
-        //                 <div className={this.state.journalShow}>
-        //                     <button onClick={() => this.setState({ journalShow: "journal_goal_hidden" })}> X </button>
-        //                     {<JournalShowContainer journal={this.state.journal} />}
-        //                 </div>
-        //                     {journalsArr.map(journal => {
-        //                         return (
-        //                         <div key={journal._id}>
-        //                             <div onClick={() => this.setState({journal: journal, journalShow: "journal_goal_show"})}>
-        //                                 {journal.createdAt.slice(0, 10)}{" "}
-        //                                 {journal.success === true ? "Step Success" : "Step Missed"}
-        //                             </div>
-        //                         </div>)
-        //                     })}
-        //             </div>
-        //             <form onSubmit={() => this.addJournal()} className={this.state.journalForm}>
-        //                 <div className="journal_radio">
-        //                     <label>Did achieve your goal step?</label>
-        //                     <div className="journal_radio_buttons">
-        //                          <p>Yes I did!
-        //                             <input type="radio" name="success" value="true" onClick={() => this.setState({success: true})}/>
-        //                          </p>
-        //                         <p>No, but I will next time!
-        //                             <input type="radio" name="success" value="false" onClick={() => this.setState({ success: false })}/>
-        //                         </p>
-        //                     </div>
-        //                 </div>
-        //                 <div className="journal_text_area">
-        //                     <label className="">Journal about your Goal!
-        //                     </label>
-        //                     <textarea className="journal_text_area_input" id="journal_input" value={this.state.body} onChange={this.handlechange("body")}/>
-        //                 </div>
-        //                 <div className="journal_text_area">
-        //                     <label>Add any highlights:
-        //                     </label>
-        //                     <input type="text" id="journal_input" value={this.state.highlights} onChange={this.handlechange("highlights")}/>
-        //                 </div>
-        //                 <div className="journal_text_area">
-        //                     <label>Add any Cues or distractions:</label>
-        //                     <input type="text" id="journal_input" value={this.state.cues} onChange={this.handlechange("cues")}/>
-        //                 </div>
-        //                 <div className="journal_text_area">
-        //                     <label>Add any rewards you gave yourself:</label>
-        //                     <input type="text" id="journal_input" value={this.state.rewards} onChange={this.handlechange("rewards")} />
-        //                     <div className="journal_button_div">
-        //                         <button className="add_journal_button" type="submit">Create New Journal</button>
-        //                     </div>
-        //                 </div>
-        //             </form>
-        //                 <button className="add_journal_button" onClick={() => this.state.journalForm === "journal_form_hidden" ? this.setState({ journalForm: "journal_form_show" }) : this.setState({ journalForm: "journal_form_hidden" })}>Add New Journal</button>
-        //         </div>
-        //    </div>
+      
           <div>
           <Hero header={"Current Goal: " + goal.title}/>
             <section className="middle taskList">
