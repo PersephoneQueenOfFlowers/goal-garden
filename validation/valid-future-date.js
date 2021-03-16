194a78aecb4565269d03de15521aceae968e16cb
Andrew Curd <andrewc2695@gmail.com>
2021-03-16 09:09:45 -0700

    creating a goal will now start on the current date and checking if hte date is valid works properly

--- validation/valid-future-date.js
+++ validation/valid-future-date.js
@@ -1,6 +1,6 @@
 const validFutureDate = dateISO => {
   if (typeof dateISO != "string") return false;
-  const date = new Date(dateISO);
+  const date = new Date(dateISO + "T00:00:00");
   if (date == "Invalid Date") return false;
   return date > Date.now();
 }
